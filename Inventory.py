28cfe48c7c07aa83698988e8e3ebffec63950426
Conner Bell <chb263@msstate.edu>
2023-11-17 18:54:55 -0600

    Create Inventory.py

---
+++ Inventory.py
@@ -0,0 +1,27 @@
+class Inventory:
+    def __init__(self):
+        self.items = {}  # Dictionary to store items with their location as the key
+
+    def add_item(self, item, location):
+        # Adds an item into the inventory at a location
+        pass
+
+    def remove_item(self, item, location):
+        # Removes an item from the inventory at a location
+        pass
+
+    def edit_item(self, item, location):
+        # Edits an item in the inventory at a location
+        pass
+
+    def get_item(self, location):
+        # Returns an item from a location
+        pass
+
+    def list_out_of_stock(self):
+        # Lists items that are out of stock
+        pass
+
+    def search_items(self, filter):
+        # Searches items based on a given filter
+        pass
