c7a29c4b179c4dd5a9ee89dab0d1193fb9e2fe68
SovaCoda <80545648+SovaCoda@users.noreply.github.com>
2023-11-29 17:15:52 -0600

    finished up shopping cart stuff
    shopping cart is now functional but needs inventory to be completed

--- ShoppingCart.py
+++ ShoppingCart.py
@@ -1,28 +1,74 @@
+from db import create_connection
+
 class ShoppingCart:
-    def __init__(self, user):
-        self.user = user
-        self.items = []  # List to store items in the cart
+    def __init__(self, database_name, table_name):
+        self.database_name = database_name
+        self.table_name = table_name
 
-    def add_item(self, item, quantity):
-        # Adds an item to the cart
-        pass
+    def set_database_table(self, database_name, table_name):
+        self.database_name = database_name
+        self.table_name = table_name
 
-    def remove_item(self, item):
-        # Removes an item from the cart
-        pass
+    def view_cart(self):
+        try:
+            conn = create_connection(self.database_name)
+            cur = conn.cursor()
+            cur.execute(f"SELECT * FROM {self.table_name} WHERE UserID = ?", (self.user_id,))
+            print(cur.fetchall())
+        except Exception as e:
+            print(f"Error occurred while viewing cart: {str(e)}")
+        finally:
+            if conn:
+                conn.close()
 
-    def adjust_item_quantity(self, item, quantity):
-        # Changes the quantity of an item in the cart
-        pass
+    def add_to_cart(self, isbn, userID, quantity):
+        try:
+            conn = create_connection(self.database_name)
+            cur = conn.cursor()
+            cur.execute(f"SELECT * FROM {self.table_name} WHERE ISBN = ? AND UserID = ?", (isbn, userID))
+            result = cur.fetchone()
+            if result:
+                existing_quantity = result[2]
+                new_quantity = existing_quantity + quantity
+                cur.execute(f"UPDATE {self.table_name} SET Quantity = ? WHERE ISBN = ? AND UserID = ?", (new_quantity, isbn, userID))
+            else:
+                cur.execute(f"INSERT INTO {self.table_name} (ISBN, UserID, Quantity) VALUES (?, ?, ?)", (isbn, userID, quantity))
+            conn.commit()
+            print("Item added to cart successfully.")
+        except Exception as e:
+            print(f"Error occurred while adding item to cart: {str(e)}")
+        finally:
+            if conn:
+                conn.close()
 
     def checkout(self):
-        # Checks out and creates orders for the items in the cart
-        pass
+        try:
+            conn = create_connection(self.database_name)
+            cur = conn.cursor()
+            cur.execute(f"SELECT ISBN, Quantity FROM {self.table_name}")
+            cart_items = cur.fetchall()
+            for item in cart_items:
+                isbn = item[0]
+                quantity = item[1]
+                cur.execute(f"UPDATE Inventory SET Stock = Stock - ? WHERE ISBN = ?", (quantity, isbn))
+            cur.execute(f"DELETE FROM {self.table_name}")
+            conn.commit()
+            print("Checkout completed successfully.")
+        except Exception as e:
+            print(f"Error occurred while checking out: {str(e)}")
+        finally:
+            if conn:
+                conn.close()
 
-    def decrease_stock(self, inventory):
-        # Adjusts the stock in the Inventory for checked-out items
-        pass
-
-    def clear(self):
-        # Clears the cart
-        pass
+    def remove_from_cart(self, isbn, userID):
+        try:
+            conn = create_connection(self.database_name)
+            cur = conn.cursor()
+            cur.execute(f"DELETE FROM {self.table_name} WHERE ISBN = ? AND UserID = ?", (isbn, userID))
+            conn.commit()
+            print("Item removed from cart successfully.")
+        except Exception as e:
+            print(f"Error occurred while removing item from cart: {str(e)}")
+        finally:
+            if conn:
+                conn.close()
